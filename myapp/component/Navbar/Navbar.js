981007fdbecbc25f550beb42a73e38880edc0cd3
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-11-01 13:54:01 -0400

    update

---
+++ myapp/component/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='px-4 py-8'>
+        <div className='flex justify-between items-center max-w-screen-xl mx-auto'>
+            Header
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
